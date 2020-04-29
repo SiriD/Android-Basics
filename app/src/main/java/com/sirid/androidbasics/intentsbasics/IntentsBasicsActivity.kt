d3cba32176f3e207f933f6055a87a2365d853b98
SiriD <sirisha.66@gmail.com>
2020-04-29 15:37:18 -0500

    added intent examples

---
+++ app/src/main/java/com/sirid/androidbasics/intentsbasics/IntentsBasicsActivity.kt
@@ -0,0 +1,32 @@
+package com.sirid.androidbasics.intentsbasics
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import com.sirid.androidbasics.R
+
+class IntentsBasicsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intents_basics)
+        val buttonExplicit = findViewById<Button>(R.id.buttonExplicit)
+        val buttonImplicit = findViewById<Button>(R.id.buttonImplicit)
+
+        // explicit intent
+        buttonExplicit.setOnClickListener {
+            val intent = Intent(this, NewIntentActivity::class.java)
+            startActivity(intent) }
+
+        // implicit intent
+        buttonImplicit.setOnClickListener {
+            val webIntent = Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"))
+            startActivity(webIntent)
+        }
+
+
+    }
+}
